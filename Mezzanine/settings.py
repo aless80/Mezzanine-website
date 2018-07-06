ff2452df7cd6b13f8eef544a996fab31c4215b4c
Alessandro <AlessandroMarin80@gmail.com>
2018-07-06 19:59:38 -0400

    Fri Jul  6 19:59:38 EDT 2018

--- Mezzanine/settings.py
+++ Mezzanine/settings.py
@@ -162,9 +162,11 @@ DATABASES = {
 #########
 
 # Full filesystem path to the project.
-PROJECT_APP_PATH = os.path.dirname(os.path.abspath(__file__))
-PROJECT_APP = os.path.basename(PROJECT_APP_PATH)
-PROJECT_ROOT = BASE_DIR = os.path.dirname(os.path.dirname(PROJECT_APP_PATH))
+PROJECT_APP_PATH = os.path.dirname(os.path.abspath(__file__))       #/home/amarin/Mezzanine-website/Mezzanine
+PROJECT_APP = os.path.basename(PROJECT_APP_PATH)                    #Mezzanine
+#PROJECT_ROOT = os.path.dirname(os.path.dirname(PROJECT_APP_PATH))   #/home/amarin
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__))) #/home/amarin/Mezzanine-website
+PROJECT_ROOT=BASE_DIR
 
 # Every cache key will get prefixed with this value - here we set it to
 # the name of the directory the project is in to try and use something
@@ -179,7 +181,7 @@ STATIC_URL = "/static/"
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = os.path.join(PROJECT_ROOT, STATIC_URL.strip("/"))
+STATIC_ROOT = os.path.join(BASE_DIR, STATIC_URL.strip("/"))
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
@@ -189,6 +191,21 @@ MEDIA_URL = STATIC_URL + "media/"
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/home/media/media.lawrence.com/media/"
 MEDIA_ROOT = os.path.join(PROJECT_ROOT, *MEDIA_URL.strip("/").split("/"))
+
+file = open('/home/amarin/Mezzanine-website/test.txt', 'w')
+file.write("BASE_DIR:\n")
+file.write("%s\n" % BASE_DIR)
+file.write("PROJECT_ROOT:\n")
+file.write("%s\n" % PROJECT_ROOT)
+file.write("STATIC_URL:\n")
+file.write("%s\n" % STATIC_URL)
+file.write("STATIC_ROOT:\n")
+file.write("%s\n" % STATIC_ROOT)
+file.write("MEDIA_URL:\n")
+file.write("%s\n" % MEDIA_URL)
+file.write("MEDIA_ROOT:\n")
+file.write("%s\n" % MEDIA_ROOT)
+file.close()
 
 # Package/module name to import the root urlpatterns from for the project.
 ROOT_URLCONF = "%s.urls" % PROJECT_APP
@@ -231,6 +248,8 @@ if DJANGO_VERSION < (1, 9):
 INSTALLED_APPS = (
     "resume",
     "moderna",
+    # "flat", #nice animation for projects
+    #"nova",
     "categories",
     "categories.editor",
     "django.contrib.admin",
@@ -378,7 +397,7 @@ else:
     EMAIL_HOST_PASSWORD=os.environ.get('EMAIL_HOST_PASSWORD')
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "moderna"),
+    os.path.join(PROJECT_ROOT, "moderna"),
 ]
 
 TEMPLATE_DIRS = [
